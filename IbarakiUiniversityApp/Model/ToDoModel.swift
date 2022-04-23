75c7f93c6f69110086efb38fcc21679d4b61772c
hirokaissh <hirokai.saito1014@gmail.com>
2022-04-23 16:55:14 +0900

    [remove]不要な箇所の削除

--- IbarakiUiniversityApp/Model/ToDoModel.swift
+++ IbarakiUiniversityApp/Model/ToDoModel.swift
@@ -8,23 +8,6 @@
 import Foundation
 import RealmSwift
 
-protocol ExchangeData {
-    func receveData ()
-    func tranceData ()
-}
-
 class ToDoModel: Object {
     @objc dynamic var labTODO: String?
-}
-
-class ExchangeToDoModel: ExchangeData {
-    var toDoItems: Results<ToDoModel>!
-
-    func receveData() {
-        <#code#>
-    }
-
-    func tranceData() {
-        <#code#>
-    }
 }
