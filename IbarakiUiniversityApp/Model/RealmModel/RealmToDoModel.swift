d71ebbd0b65f565ee57cc7dcc1e5b2c99f5a1b2b
hirokaissh <hirokai.saito1014@gmail.com>
2022-04-25 20:37:24 +0900

    [add] RealmToDoModelの作成

--- IbarakiUiniversityApp/Model/RealmModel/RealmToDoModel.swift
+++ IbarakiUiniversityApp/Model/RealmModel/RealmToDoModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import RealmSwift
+
+class RealmToDo: Object {
+    @objc dynamic var uuidString = ""
+    @objc dynamic var labTODO: String?
+
+    var uuid: UUID? {
+        UUID(uuidString: uuidString)
+    }
+
+    override class func primaryKey() -> String? {
+        "uuidString"
+    }
+
+    convenience init(labToDo: String) {
+        self.init()
+        self.labTODO = labToDo
+    }
+}
+
+private extension SwiftLabToDo {
+    init(managedObject: RealmToDo) {
+        self.uuidString = managedObject.uuidString
+        self.labToDo = managedObject.labTODO
+    }
+
+    func managedObject() -> RealmToDo {
+        let realmToDo = RealmToDo()
+        realmToDo.uuidString = self.uuidString
+        realmToDo.labTODO = self.labToDo
+        return realmToDo
+    }
+}
