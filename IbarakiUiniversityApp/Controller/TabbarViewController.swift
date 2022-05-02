f623f8eacd1953a19963e462c5e3dc67a58b52b3
hirokaissh <hirokai.saito1014@gmail.com>
2022-05-02 22:15:55 +0900

    [clean] バッチ処理の修正

--- IbarakiUiniversityApp/Controller/TabbarViewController.swift
+++ IbarakiUiniversityApp/Controller/TabbarViewController.swift
@@ -23,7 +23,17 @@ class TabBarViewController: UITabBarController {
         }
         let todoItems = todoRepository.loadLabToDo()
         let documentItems = documentRepository.loadDocument()
-        tabBar.items?[0].badgeValue = "\(String(todoItems.count))"
-        tabBar.items?[1].badgeValue = "\(String(documentItems.count))"
+
+        if todoItems.count == 0 {
+            tabBar.items?[1].badgeValue = nil
+        } else {
+            tabBar.items?[0].badgeValue = "\(String(todoItems.count))"
+        }
+
+        if documentItems.count == 0 {
+            tabBar.items?[0].badgeValue = nil
+        } else {
+            tabBar.items?[1].badgeValue = "\(String(documentItems.count))"
+        }
     }
 }
