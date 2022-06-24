f1347486ee49e2db3fe63cd54140ddf31bf3d354
hiroakissh <hiroaki.saito1014@gmail.com>
2022-06-24 23:30:44 +0900

    [add] TableViewに関するコードの追加

--- IbarakiUiniversityApp/Controller/showWebSiteViewController.swift
+++ IbarakiUiniversityApp/Controller/showWebSiteViewController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class showWebSiteViewController: UIViewController {
 
+    @IBOutlet private weak var classTableView: UITableView!
+    @IBOutlet private weak var homeTableView: UITableView!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+        classTableView.delegate = self
+        classTableView.dataSource = self
+        homeTableView.delegate = self
+        homeTableView.dataSource = self
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+}
+
+extension showWebSiteViewController: UITableViewDelegate {
+
+}
+
+extension showWebSiteViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
 
 }
