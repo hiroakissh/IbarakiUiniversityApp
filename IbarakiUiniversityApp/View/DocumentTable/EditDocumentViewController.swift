0f6576516da936d5019b1de0ede4258110d01b6d
hiroakissh <hiroaki.saito1014@gmail.com>
2023-01-28 21:15:17 +0900

    [add] 編集時に既存の値を表示する機能の追加

--- IbarakiUiniversityApp/View/DocumentTable/EditDocumentViewController.swift
+++ IbarakiUiniversityApp/View/DocumentTable/EditDocumentViewController.swift
@@ -8,14 +8,27 @@
 import UIKit
 
 class EditDocumentViewController: UIViewController {
+    @IBOutlet private weak var documentNameTextField: UITextField!
+    @IBOutlet private weak var documentDataPicker: UIDatePicker!
 
     var editDocumentUUID: String?
     var documentRepository = DocumentRepository()
 
+    var documentSwiftModel: SwiftDocumentModel?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(editDocumentUUID)
         guard let editDocumentUUID = editDocumentUUID else { return }
-        print(documentRepository.loadDocumentOfUUID(uuid: editDocumentUUID))
+        documentSwiftModel = documentRepository.loadDocumentOfUUID(uuid: editDocumentUUID)
+        documentNameTextField.text = documentSwiftModel?.documentTitle
+        let deadLine = documentSwiftModel?.deadLine
+        guard let deadLine = deadLine else { return }
+        documentDataPicker.date = deadLine
+    }
+
+    @IBAction private func uodateDocument(_ sender: Any) {
+    }
+    @IBAction private func backButton(_ sender: Any) {
+        dismiss(animated: true)
     }
 }
